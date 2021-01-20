a4ef12ede0c95071f82c5a9021dae5de08d0851a
CycleWeeb <dmitry.a.vakulenko@gmail.com>
2021-01-19 23:00:51 -0500

    did some styles or something i need sleep

--- index.js
+++ index.js
@@ -22,14 +22,28 @@ function createWindow() {
         //icon: getPlatformIcon('Logo'),
         frame: false,
         // preloader shit
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+            worldSafeExecuteJavaScript: true
+        },
         backgroundColor: '#171614'
     });
+
+    // TODO: background image selector with ejse
 
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'app', 'app.ejs'),
         protocol: 'file:',
         slashes: true
     }));
+
+    win.removeMenu();
+    win.resizable = true;
+    win.on('closed', () => {
+        win = null;
+    });
 }
 
 function createMenu() {
@@ -111,3 +125,19 @@ function getPlatformIcon(filename) {
 
 app.on('ready', createWindow);
 app.on('ready', createMenu);
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+})
